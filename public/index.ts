f0746b30417e86639ddd62aa5f5a908382224e80
zy <webyangfullstack@gmail.com>
2024-09-25 18:06:41 +1000

    update

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,5 @@
 // import Logo from './midas-logo.png'
-import Logo from './logo-03.png'
+import Logo from './mm-new.png'
 import HomeBg from './Home-bg.png'
 import HomeValue from './home-value.png'
 import AboutBg from './About-bg.png'
